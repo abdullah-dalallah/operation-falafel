e21cddbf2e84940eb916ea509ea711cf8a5de6fa
abdullhali53 <62836999+abdullah-dalallah@users.noreply.github.com>
2022-11-04 12:38:43 +0400

    remove video

--- lib/screens/item_details_page/item_details_sheet.dart
+++ lib/screens/item_details_page/item_details_sheet.dart
@@ -58,8 +58,7 @@ class _ItemDetailsSheetState extends State<ItemDetailsSheet> {
 
 
 
-  late VideoPlayerController _controller;
-  late Future<void> _initializeVideoPlayerFuture;
+
 
   @override
   void initState() {
@@ -68,70 +67,12 @@ class _ItemDetailsSheetState extends State<ItemDetailsSheet> {
     print(itemOptions['Add on']?.length);
     print(itemOptions[itemOptions.keys.elementAt(1)][1]['${itemOptions[itemOptions.keys.elementAt(1)][1].keys.elementAt(0)}']["Id"]);
 
-    // Create and store the VideoPlayerController. The VideoPlayerController
-    // offers several different constructors to play videos from assets, files,
-    // or the internet.
-    _controller = VideoPlayerController.network(
-      videoPlayerOptions: VideoPlayerOptions(
 
-      ),
-
-      'https://clipchamp.com/watch/tehjHhBuWKS',
-    );
-
-    _initializeVideoPlayerFuture = _controller.initialize();
 
   }
 
 
-  showAlertDialog(BuildContext context) {
 
-    // set up the button
-    Widget okButton = TextButton(
-      child: Text("OK"),
-      onPressed: () {
-        _controller.value.isPlaying
-            ? _controller.pause()
-            : _controller.play();
-      },
-    );
-
-    // set up the AlertDialog
-    AlertDialog alert = AlertDialog(
-      title: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the VideoPlayerController has finished initialization, use
-            // the data it provides to limit the aspect ratio of the video.
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              // Use the VideoPlayer widget to display the video.
-              child: VideoPlayer(_controller),
-            );
-          } else {
-            // If the VideoPlayerController is still initializing, show a
-            // loading spinner.
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
-      content: Text("This is my message."),
-      actions: [
-        okButton,
-      ],
-    );
-
-    // show the dialog
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alert;
-      },
-    );
-  }
 
 
   @override
@@ -391,16 +332,7 @@ class _ItemDetailsSheetState extends State<ItemDetailsSheet> {
                             height: 45,
                             child: ElevatedButton(
                                 onPressed: (){
-                                  setState(() {
-                                    // If the video is playing, pause it.
-                                    if (_controller.value.isPlaying) {
-                                      _controller.pause();
-                                    } else {
-                                      // If the video is paused, play it.
-                                      _controller.play();
-                                    }
-                                  });
-                                  showAlertDialog( context);
+
                                 },
                                 style: ButtonStyle(
                                   backgroundColor:  MaterialStateProperty.all<Color>(Color(0xFF84ab24)),
